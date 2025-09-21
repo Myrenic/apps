221851a33cc33445eb7462b7ffd6baf42901c95f
Myrenic <mike.luchies@gmail.com>
2025-09-21 10:30:21 +0000

    init

---
+++ images/services/sonarr/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+CONFIG_FILE="/config/config.xml"
+
+# Patch AuthenticationMethod if EXTERNAL_AUTH=true
+if [ "$EXTERNAL_AUTH" = "true" ] && [ -f "$CONFIG_FILE" ]; then
+    if grep -q "<AuthenticationMethod>" "$CONFIG_FILE"; then
+        sed -i 's|<AuthenticationMethod>.*</AuthenticationMethod>|<AuthenticationMethod>External</AuthenticationMethod>|' "$CONFIG_FILE"
+    else
+        # Add it under <Config> root
+        sed -i '0,/<Config>/ s|<Config>|<Config>\n  <AuthenticationMethod>External</AuthenticationMethod>|' "$CONFIG_FILE"
+    fi
+fi
+
+# Set container timezone
+if [ -n "$TZ" ]; then
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+    echo "$TZ" > /etc/timezone
+fi
+
+# Start Sonarr
+exec dotnet /app/Sonarr.dll --no-browser --data=/config
